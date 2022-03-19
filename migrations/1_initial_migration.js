1b4e224501c5e246fd7c811cc816c1fa5300565f
Joyce Hong <joyceHong0524@gmail.com>
2022-03-19 16:22:16 +0900

    Initial Set up

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,7 @@
+const Migrations = artifacts.require('Migrations');
+const KlaytnGreeter = artifacts.require('./KlaytnGreeter.sol');
+
+module.exports = function (deployer) {
+    deployer.deploy(Migrations);
+    deployer.deploy(KlaytnGreeter, 'hello joyce');
+};
---
+++ truffle-config.js
@@ -0,0 +1,119 @@
+const HDWalletProvider = require('truffle-hdwallet-provider-klaytn');
+const Caver = require('caver-js');
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+    networks: {
+        development: {
+            host: 'localhost',
+            port: 8545,
+            network_id: '*' // Match any network id
+        },
+        klaytn: {
+            provider: () => {
+                const pks = JSON.parse(
+                    fs.readFileSync(path.resolve(__dirname) + '/privateKeys.js')
+                );
+
+                return new HDWalletProvider(
+                    pks,
+                    'http://localhost:8551',
+                    0,
+                    pks.length
+                );
+            },
+            network_id: '1001', //Klaytn baobab testnet's network id
+            gas: '8500000',
+            gasPrice: null
+        },
+        kasBaobab: {
+            provider: () => {
+                const option = {
+                    headers: [
+                        {
+                            name: 'Authorization',
+                            value:
+                                'Basic ' +
+                                Buffer.from(
+                                    process.env.KAS_ACCESSKEY_ID +
+                                        ':' +
+                                        process.env.KAS_SECRET_ACCESS_KEY
+                                ).toString('base64')
+                        },
+                        { name: 'x-chain-id', value: '1001' }
+                    ],
+                    keepAlive: false
+                };
+                return new HDWalletProvider(
+                    process.env.PRIVATE_KEY,
+                    new Caver.providers.HttpProvider(
+                        'https://node-api.klaytnapi.com/v1/klaytn',
+                        option
+                    )
+                );
+            },
+            network_id: '1001', //Klaytn baobab testnet's network id
+            gas: '8500000',
+            gasPrice: null
+        },
+        kasCypress: {
+            provider: () => {
+                const option = {
+                    headers: [
+                        {
+                            name: 'Authorization',
+                            value:
+                                'Basic ' +
+                                Buffer.from(
+                                    process.env.KAS_ACCESSKEY_ID +
+                                        ':' +
+                                        process.env.KAS_SECRET_ACCESS_KEY
+                                ).toString('base64')
+                        },
+                        { name: 'x-chain-id', value: '8217' }
+                    ],
+                    keepAlive: false
+                };
+                return new HDWalletProvider(
+                    process.env.PRIVATE_KEY,
+                    new Caver.providers.HttpProvider(
+                        'https://node-api.klaytnapi.com/v1/klaytn',
+                        option
+                    )
+                );
+            },
+            network_id: '8217', //Klaytn baobab testnet's network id
+            gas: '8500000',
+            gasPrice: null
+        },
+        baobab: {
+            provider: () => {
+                return new HDWalletProvider(
+                    process.env.PRIVATE_KEY,
+                    'https://api.baobab.klaytn.net:8651'
+                );
+            },
+            network_id: '1001', //Klaytn baobab testnet's network id
+            gas: '8500000',
+            gasPrice: null
+        }
+        // cypress: {
+        //     provider: () => {
+        //         return new HDWalletProvider(
+        //             process.env.PRIVATE_KEY,
+        //             'NO CUSTOM EN'
+        //         );
+        //     },
+        //     network_id: '8217', //Klaytn mainnet's network id
+        //     gas: '8500000',
+        //     gasPrice: null
+        // }
+    },
+
+    compilers: {
+        solc: {
+            version: '0.8.9'
+        }
+    }
+};
