746c7893d2b44a5c966b8a88569ecd378029906f
CC <0xcrispycake@gmail.com>
2022-03-21 21:13:54 +0900

    Added VotingKSP interface

---
+++ contracts/interfaces/klayswap/IVotingKSP.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+interface IVotingKSP {
+    function lockKSP(uint amount, uint lockPeriodRequested) external;
+    function unlockKSP() external;
+    function refixBoosting(uint lockPeriodRequested) external;
+    function claimReward() external;
+    function compoundReward() external;
+}
