676331cef9f62d39264343ebf7fed10ef52fc7c0
CC <0xcrispycake@gmail.com>
2022-03-29 14:23:32 +0900

    SigKSPStaking test case done

--- test/SigKSPStaking.js
+++ test/SigKSPStaking.js
@@ -22,7 +22,7 @@ const MAX_UINT_256 = new BN(2).pow(new BN(256)).sub(new BN(1));
 
 contract('SigKSPStaking', function (accounts) {
     describe('SigKSP Staking Test.', () => {
-        //--network baobab 이나 local 만 됨.
+        //--network baobab 이나 local 만 테스트 가능.
         let userA = accounts[0];
         let userB = accounts[1];
 
@@ -39,9 +39,21 @@ contract('SigKSPStaking', function (accounts) {
             );
             console.log('Deploy Token and Logic smart contract');
 
-            sigKSPToken = await makeErc20Token(); //Staking token
-            SIGToken = await makeErc20Token(); //Reward 1
-            KSPToken = await makeErc20Token(); //Reward 2
+            //Deploy from the start.
+            // sigKSPToken = await makeErc20Token(); //Staking token
+            // SIGToken = await makeErc20Token(); //Reward 1
+            // KSPToken = await makeErc20Token(); //Reward 2
+
+            //Use deployed one. (baobab)
+            sigKSPToken = await MockERC20.at(
+                '0x68C8fD98ba4C958FaD3011fc632DE2bc912C46db'
+            ); //Staking token
+            SIGToken = await MockERC20.at(
+                '0x14e8C9052ae53b6e26aC3953E7C31Ec5cC2eEE13'
+            ); //Reward 1
+            KSPToken = await MockERC20.at(
+                '0xe41CE2a5dfb9f9B17500De284Ac98A51e03676eA'
+            ); //Reward 2
 
             console.log('sigKSPToken : ', sigKSPToken.address);
             console.log('SIGToken : ', SIGToken.address);
@@ -60,7 +72,7 @@ contract('SigKSPStaking', function (accounts) {
             ]);
 
             //3. check if there is no ownership after setAdddress.
-            expectEqual(await sigKSPStaking.owner(), address(0));
+            expectEqual(await sigKSPStaking.owner(), userA);
 
             //4. check if there are two reward token.
             expectEqual(await sigKSPStaking.rewardTokens(0), SIGToken.address);
@@ -97,9 +109,181 @@ contract('SigKSPStaking', function (accounts) {
             );
         });
 
-        it('UserA and userB get sigKSP tokens.', async () => {
-            //1. Mint 100 sigKSP to UserA
+        it('Update Reward Amount', async () => {
+            //Revert Reason is detectable only on --network development,
+            //if you are testing on klaytn testnet, user expectRevert.unspecified()
+
+            // await expectRevert(
+            //     sigKSPStaking.updateRewardAmount(),
+            //     'Caller is not RewardsDistribution contract'
+            // );
+
+            // If setRewardsDistribution has not been set yet, it reverts.
+            await expectRevert.unspecified(sigKSPStaking.updateRewardAmount());
+
+            await sigKSPStaking.setRewardsDistribution(userA);
+
+            let receipt = await sigKSPStaking.updateRewardAmount();
+
+            expectEvent(receipt, 'RewardAdded', {
+                rewardsToken: SIGToken.address,
+                reward: bnMantissa(100)
+            });
+
+            expectEvent(receipt, 'RewardAdded', {
+                rewardsToken: KSPToken.address,
+                reward: bnMantissa(2000)
+            });
+
+            const SIGTokenRewardStruct = await sigKSPStaking.rewardData(
+                SIGToken.address
+            );
+
+            const KSPTokenRewardStruct = await sigKSPStaking.rewardData(
+                KSPToken.address
+            );
+
+            printReward(SIGTokenRewardStruct);
+            printReward(KSPTokenRewardStruct);
+        });
+
+        it('UserA and userB mint sigKSP and stake it', async () => {
+            //1. Mint 100 sigKSP to UserA and stake.
+            await sigKSPToken.mint(bnMantissa(100), { from: userA });
+
+            //approve first
+            await sigKSPToken.approve(sigKSPStaking.address, MAX_UINT_256, {
+                from: userA
+            });
+
+            let receipt = await sigKSPStaking.stake(bnMantissa(100), {
+                from: userA
+            });
+
+            expectEvent(receipt, 'Staked', {
+                user: userA,
+                amount: bnMantissa(100)
+            });
+
+            let SIGTokenRewardStruct = await sigKSPStaking.rewardData(
+                SIGToken.address
+            );
+
+            let KSPTokenRewardStruct = await sigKSPStaking.rewardData(
+                KSPToken.address
+            );
+
+            // Just check SIG
+            const rewardPerTokenStoredAfterUserAStake = SIGTokenRewardStruct[3];
+
+            printReward(SIGTokenRewardStruct);
+            printReward(KSPTokenRewardStruct);
+
             //2. Mint 200 sigKSP to UserB
+            await sigKSPToken.mint(bnMantissa(200), { from: userB });
+
+            //approve first
+            await sigKSPToken.approve(sigKSPStaking.address, MAX_UINT_256, {
+                from: userB
+            });
+
+            receipt = await sigKSPStaking.stake(bnMantissa(200), {
+                from: userB
+            });
+
+            expectEvent(receipt, 'Staked', {
+                user: userB,
+                amount: bnMantissa(200)
+            });
+
+            SIGTokenRewardStruct = await sigKSPStaking.rewardData(
+                SIGToken.address
+            );
+
+            KSPTokenRewardStruct = await sigKSPStaking.rewardData(
+                KSPToken.address
+            );
+
+            printReward(SIGTokenRewardStruct);
+            printReward(KSPTokenRewardStruct);
+
+            //3. Check userA and userB userRewardPerTokenPaid
+            // 3-1) userA's userRewardPerTokenPaid should be 0
+            expectEqual(
+                await sigKSPStaking.userRewardPerTokenPaid(
+                    userA,
+                    SIGToken.address
+                ),
+                0
+            );
+            // 3-1) userB's userRewardPerTokenPaid should be "rewardPerTokenStoredAfterUserAStake + pending"
+            expect(
+                (await sigKSPStaking.userRewardPerTokenPaid(
+                    userB,
+                    SIGToken.address
+                )) > rewardPerTokenStoredAfterUserAStake
+            ).to.be.ok;
+        });
+
+        it('userA claim reward', async () => {
+            //Test with SIG Token.
+            //1. Check user's SIG Balance.
+            console.log(
+                '[Before Claim] UserA SIG Token Balance : ',
+                (await SIGToken.balanceOf(userA)).toString()
+            );
+            //2. Check user's Earned
+            expectEqual(
+                await sigKSPStaking.rewards(userA, SIGToken.address),
+                0
+            );
+            let receipt = await sigKSPStaking.getReward({ from: userA });
+
+            console.log(
+                '[After Claim] UserA SIG Token Balance : ',
+                (await SIGToken.balanceOf(userA)).toString()
+            );
+        });
+
+        it('userB exit(withdraw)', async () => {
+            //Test with KSP Token
+            //1. Check user's KSP Balance.
+            console.log(
+                '[Before Claim] UserB KSP Token Balance : ',
+                (await KSPToken.balanceOf(userB)).toString()
+            );
+
+            let receipt = await sigKSPStaking.exit({ from: userB });
+
+            expectEvent(receipt, 'UpdateReward', {
+                rewardToken: KSPToken.address,
+                userRewardPerTokenPaid: (
+                    await sigKSPStaking.rewardData(KSPToken.address)
+                )[3]
+            });
+
+            expectEvent(receipt, 'Withdrawn', {
+                user: userB
+            });
+
+            console.log(
+                '[After Claim] UserB KSP Token Balance : ',
+                (await KSPToken.balanceOf(userB)).toString()
+            );
         });
     });
 });
+
+function printReward(rewardData) {
+    console.log(
+        `\t\t\tReward Data \t\t\t\t{\n period Finish : ${new BN(
+            rewardData[0]
+        ).toString()} \n\t\t\t\t rewardRate : ${new BN(
+            rewardData[1]
+        ).toString()}\n\t\t\t\t lastUpdateTime : ${new BN(
+            rewardData[2]
+        ).toString()}\n\t\t\t\t rewardPerTokenStored : ${new BN(
+            rewardData[3]
+        ).toString()}\n\t\t\t\t balance : ${new BN(rewardData[4]).toString()}`
+    );
+}
