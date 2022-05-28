6d1c8a75875f97f5852a87fc7fb3e2039b2b53ab
CC <0xcrispycake@gmail.com>
2022-05-28 18:27:16 +0900

    import openzeppelin contract

--- contracts/lockdrop/Lockdrop.sol
+++ contracts/lockdrop/Lockdrop.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
-import "../dependencies/Ownable.sol";
-import "../dependencies/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Lockdrop is Ownable {
